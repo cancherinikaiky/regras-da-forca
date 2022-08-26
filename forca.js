a7fab22807d95d6b7b254aa71c5aeb6c031aeed9
Kaiky Canherini <100038715+cancherinikaiky@users.noreply.github.com>
2022-08-26 13:30:47 -0300

    simplify methods and aplicate clean code

--- forca.js
+++ forca.js
@@ -1,111 +1,105 @@
 class Forca {
-  constructor(palavraSecreta) {
-    this._palavraSecreta = this.palavraParaArray(palavraSecreta);
-    this._palavraJogada = this.ocultarPalavra(this._palavraSecreta);
-    this._acertos = 0;
-    this._chutes = [];
-    this._vidasRestantes = 6;
-  }
-
-  // Getters
-  get palavraSecreta() {
-    return this._palavraSecreta;
-  }
-  get palavraJogada() {
-    return this._palavraJogada;
-  }
-  get acertos() {
-    return this._acertos;
-  }
-  get chutes() {
-    return this._chutes;
-  }
-  get vidasRestantes() {
-    return this._vidasRestantes;
-  }
-
-  // Converte palavra para array onde cada letra é uma posição
-  palavraParaArray(palavra) {
-    return palavra.split("");
-  }
-
-  // Cria a palavra oculta do mesmo tamanho que a palavra secreta
-  ocultarPalavra(palavra) {
-    const palavraJogada = [];
-    for (let i = 0; i < palavra.length; i++) {
-      palavraJogada[i] = "_";
+    constructor(palavraSecreta) {
+      this._palavraSecreta = this.palavraParaArray(palavraSecreta);
+      this._palavraJogada = this.ocultarPalavra(this._palavraSecreta);
+      this._acertos = 0;
+      this._chutes = [];
+      this._vidasRestantes = 6;
     }
-    return palavraJogada;
-  }
+  
+    get palavraSecreta() {
+      return this._palavraSecreta;
+    }
+    get palavraJogada() {
+      return this._palavraJogada;
+    }
+    get acertos() {
+      return this._acertos;
+    }
+    get chutes() {
+      return this._chutes;
+    }
+    get vidasRestantes() {
+      return this._vidasRestantes;
+    }
+  
+    palavraParaArray(palavra) {
+      return palavra.split("");
+    }
+  
+    ocultarPalavra(palavra) {
+      const palavraOculta = [];
+      for (let i = 0; i < palavra.length; i++) {
+        palavraOculta[i] = "_";
+      }
+      return palavraOculta;
+    }
+  
+    letraNaoChutada(chute) {
+      return this.chutes.indexOf(chute) === -1;
+    }
+  
+    chuteComUmaLetra(jogada) {
+      const jogadaSemEspacos = jogada.replace(/\s+/g, '');
+      return jogadaSemEspacos.length === 1;
+    }
+    
+     atualizarAcertos() {
+      this._acertos += 1;
+    }
+  
+    retirarVida() {
+      this._vidasRestantes -= 1;
+    }
+  
+    atualizarPalavraJogada(letraJogada) {
+      this.palavraSecreta.forEach((letraSecreta, i) => {
+          if (letraSecreta === letraJogada) {
+              this.palavraJogada[i] = letraJogada;
+              this.atualizarAcertos();
+          }
+      });
+    }
 
-  // Booleano que verifica se a letra já foi chutada
-  letraExistente(jogada) {
-    return this.chutes.find(letra => letra === jogada);
-  }
-
-  // Verifica se a jogada possui só uma letra
-  quantLetras(jogada) {
-    const jogadaSemEspacos = jogada.replace(/\s+/g, '');
-    return jogadaSemEspacos.length === 1;
-  }
-
-  // Atualiza a palavra quando alguma letra é acertada
-  atualizarPalavraJogada(jogada) {
-    this.palavraSecreta.forEach((letra, i) => {
-        if (letra === jogada) {
-            this.palavraJogada[i] = jogada;
-            this.atualizarAcertos();
+    chuteCorreto(chute) {
+        return this.palavraSecreta.indexOf(chute) !== -1;
+    }
+  
+    chutar(chute) {
+      const jogadaValida = this.letraNaoChutada(chute) && this.chuteComUmaLetra(chute);
+      if (jogadaValida) {
+        this._chutes.push(chute);
+        if (this.chuteCorreto(chute)) {
+          this.atualizarPalavraJogada(chute);
+        } else {
+          this.retirarVida();
         }
-    });
-  }
-
-  // Caso o jogador acerte o chute, irá adicionar um acerto
-  atualizarAcertos() {
-    this._acertos += 1;
-  }
-
-  // Caso o jogador erre o chute, irá retirar uma vida
-  retirarVida() {
-    this._vidasRestantes -= 1;
-  }
-
-  // Verifica se a jogada é válida, e caso for, executa a jogada
-  chutar(letraJogada) {
-    if (!this.letraExistente(letraJogada) && this.quantLetras(letraJogada)) {
-      this._chutes.push(letraJogada);
-      const verificarJogada = this.palavraSecreta.find(letra => letraJogada === letra);
-      if (verificarJogada) {
-        this.atualizarPalavraJogada(letraJogada);
-      } else {
-        this.retirarVida();
       }
     }
-  }
-
-  // Verifica e atualiza o estado atual do jogo
-  buscarEstado() {
-    const estado = {
-      perdeu: "perdeu",
-      ganhou: "ganhou",
-      aguardando: "aguardando chute"
-    }
-
-    if (this.vidasRestantes === 0) {
-      return estado.perdeu;
-    } else if (this.palavraSecreta.length === this.acertos) {
-      return estado.ganhou;
-    }
-    return estado.aguardando;
-  }
-
-  // Retorna os dados atualizados do jogo
-  buscarDadosDoJogo() {
-      return {
-          vidas: this.vidasRestantes, // Quantidade de vidas restantes
-          palavra: this.palavraJogada, // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-          letrasChutadas: this.chutes // Deve conter todas as letras chutadas
+  
+    buscarEstado() {
+      const estado = {
+        perdeu: "perdeu",
+        ganhou: "ganhou",
+        aguardando: "aguardando chute"
       }
-  }
-}
+  
+      if (this.vidasRestantes === 0) {
+        return estado.perdeu;
+      } else if (this.palavraSecreta.length === this.acertos) {
+        return estado.ganhou;
+      }
+      return estado.aguardando;
+    }
 
-module.exports = Forca;
+    buscarDadosDoJogo() {
+        return {
+            vidas: this.vidasRestantes,
+            palavra: this.palavraJogada,
+            letrasChutadas: this.chutes
+        }
+    }
+  }
+  
+  module.exports = Forca;
+  
